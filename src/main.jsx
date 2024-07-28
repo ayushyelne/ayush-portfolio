1e18aa902af127118be073b5d2905ccb0d6043f9
ayushyelne <a.yelne@traderverse.io>
2024-07-28 10:03:33 -0700

    minor

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { inject } from '@vercel/analytics';
+// import { inject } from '@vercel/analytics';
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <inject />
+    <Analytics />
+    {/* <inject /> */}
   </React.StrictMode>,
 )
